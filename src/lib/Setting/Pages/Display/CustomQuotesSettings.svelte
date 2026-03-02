c5ba3fc6e597bfdd04be3320aede96727ff62206
SameDesu123 <47582414+SameDesu123@users.noreply.github.com>
2026-03-02 09:19:09 +0900

    Feat: Add CustomQuotesSettings component for Display Settings

---
+++ src/lib/Setting/Pages/Display/CustomQuotesSettings.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { language } from "src/lang";
+    import { DBState } from "src/ts/stores.svelte";
+    import TextInput from "src/lib/UI/GUI/TextInput.svelte";
+</script>
+
+<span class="text-textcolor mt-4">{language.leadingDoubleQuote}</span>
+<TextInput bind:value={DBState.db.customQuotesData[0]} />
+
+<span class="text-textcolor mt-4">{language.trailingDoubleQuote}</span>
+<TextInput bind:value={DBState.db.customQuotesData[1]} />
+
+<span class="text-textcolor mt-4">{language.leadingSingleQuote}</span>
+<TextInput bind:value={DBState.db.customQuotesData[2]} />
+
+<span class="text-textcolor mt-4">{language.trailingSingleQuote}</span>
+<TextInput bind:value={DBState.db.customQuotesData[3]} />

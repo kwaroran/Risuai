6423a52b3e5fc34fe99afc077433f063aa223e3a
SameDesu123 <47582414+SameDesu123@users.noreply.github.com>
2026-05-09 19:38:19 +0900

    chore: pin pnpm v11 in automation

--- server.bat
+++ server.bat
@@ -1,4 +1,5 @@
-call npm -g install pnpm
+call corepack enable
+call corepack prepare pnpm@11.0.9 --activate
 call pnpm install
 call pnpm run build
 call pnpm run runserver
--- server.sh
+++ server.sh
@@ -1,4 +1,5 @@
-npm install -g pnpm
+corepack enable
+corepack prepare pnpm@11.0.9 --activate
 pnpm install
 pnpm run build
 pnpm run runserver
